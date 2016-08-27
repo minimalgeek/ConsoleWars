296bb18bb9c6e27590abe52419963205ea1de585
minimalgeek <minimalgeek@gmail.com>
2016-08-27 21:32:19 +0200

    Debugger

---
+++ Assets/Script/DebugTilt.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DebugTilt : MonoBehaviour {
+
+    public Text textX;
+    public Text textY;
+    public Text textZ;
+
+    void Start () {
+    }
+
+    void Update () {
+        textX.text = Input.acceleration.x + "";
+        textY.text = Input.acceleration.y + "";
+        textZ.text = Input.acceleration.z + "";
+    }
+}
