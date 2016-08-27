ef0a265b5ac28a64df19e7ef5d86c721343d09d0
minimalgeek <minimalgeek@gmail.com>
2016-08-27 18:59:29 +0200

    Audios

---
+++ Assets/Script/PausePlayManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PausePlayManager : MonoBehaviour {
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0.0f;
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+    }
+}
